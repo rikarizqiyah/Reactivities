e75a3cd1ac11383f684a403b9674841a2447a89b
rikarizqiyah <rikaraudhotul@gmail.com>
2023-04-24 15:00:48 +0700

    End of section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,13 +4,23 @@ import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { StoreContext, store } from './app/stores/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <StoreContext.Provider value={store}>
     <App />
+  </StoreContext.Provider>
 );
+
+// ReactDOM.render(
+//   <StoreContext.Provider value={store}>
+//     <App />
+//   </StoreContext.Provider>
+//   document.getElementById('root') as HTMLElement
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
